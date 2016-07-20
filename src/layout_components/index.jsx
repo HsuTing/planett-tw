6c891b66d39a2c87a303c642bcd384052216e6ac
HsuTing <hsuting0106@gmail.com>
2016-07-20 12:15:49 +0800

    new layout

--- src/layout_components/index.jsx
+++ src/layout_components/index.jsx
@@ -7,7 +7,6 @@ import Code from './../code';
 
 import * as basic from './basic';
 import * as changeTotal from './change-total';
-import * as layout from './layout';
 
 export default class LayoutPage extends React.Component {
   render() {
@@ -27,11 +26,6 @@ export default class LayoutPage extends React.Component {
         <div title={'Change total'}
              subtitle={'`default` total is `12`.'}
              data={changeTotal}
-        />
-
-        <div title={'Layout'}
-             subtitle={'You need to run `layout.run` in `componentDidMount`. `layout.run` can have three arguments. 1st argument is for `desktop size`, 2nd argument is for `tablet size` and 3rd argument is for `phone size`. `tablet size` is between 839 and 480. `phone size` is between 479 and 0. You also need to add `this.forceUpdate()` in each function to rerender components. Every `layout.style` will return new value to components. You can see <a href=\'https://www.npmjs.com/package/planett-layout\' target=\'_blank\' style=\'color: inherit;\'>here</a> to learn more.'}
-             data={layout}
         />
       </Example>
     );
